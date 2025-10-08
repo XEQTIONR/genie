5553ff1586869fcf862565b09369e4fccf0bc24d
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2025-10-08 17:26:25 +0600

    Basic layout for homepage.

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,8 +1,11 @@
+import { Input } from '@/components/ui/input';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import { Separator } from '@/components/ui/separator';
 import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { SearchCheckIcon, SearchIcon } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -11,24 +14,37 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+const items = (new Array(50)).fill(0)
+
 export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                
+                <div className="h-full md:h-[450px] flex  gap-4 rounded-xl border-sidebar-border/70 dark:border-sidebar-border">
+                    <div className="w-1/2 flex gap-3 flex-col justify-center border-sidebar-border/70 dark:border-sidebar-border">
+                        <p className="text-6xl font-bold">Find passionate people <br /> to build games with <br /> in 3 clicks.</p>
+                        <div className="w-full relative">
+                            <SearchIcon className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                            <Input className='pl-10' placeholder='What are you looking for?' />
+                        </div>
                     </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="w-1/2 border-sidebar-border/70 dark:border-sidebar-border">
+                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        </div>
                     </div>
                 </div>
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                <Separator />
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    {
+                        items.map(() => (
+                            <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                                <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </AppLayout>
