42075dbc7891c49088318eb4f612e86d6f9b44bd
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2025-10-12 23:24:51 +0600

    Add tabs component to profile

--- resources/js/pages/profile.tsx
+++ resources/js/pages/profile.tsx
@@ -13,6 +13,8 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Separator } from '@/components/ui/separator'
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -39,47 +41,59 @@ export default function Home() {
                             <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                         </div>
                 </div>
-                <div className="w-full relative -top-28 flex flex-col gap-8">
-                    <div className="w-48 ml-12 rounded-full aspect-square relative border border-sidebar-border/70 dark:border-sidebar-border">
+                <div className="w-full relative -top-14 md:-top-28 flex flex-col gap-8">
+                    <div className="w-24 md:w-48 ml-4 md:ml-12 rounded-full aspect-square relative border border-sidebar-border/70 dark:border-sidebar-border">
                         <PlaceholderPattern className="absolute rounded-full inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
-                    <div className="flex justify-between">
-                        <div className="ml-12 w-full">
-                            <div className="w-full flex justify-between items-center">
-                                <div className="text-5xl font-bold flex items-center gap-4">Ishtehar Hussain <span className="text-sm bg-primary text-background px-2 py-0.5 rounded">PRO</span></div>
-                                <div className="flex gap-1 items-center">
-                                    <span className="mr-3 text-sm">Let's build something together</span>
-                                    <Button className="cursor-pointer">Get in touch</Button>
-                                    <DropdownMenu>
-                                        <DropdownMenuTrigger asChild>
-                                            <Button className="cursor-pointer" variant="outline" size="icon"><EllipsisVertical /></Button>
-                                        </DropdownMenuTrigger>
-                                        <DropdownMenuContent sideOffset={10} className="dark:bg-neutral-900" align="end">
-                                            <DropdownMenuLabel>Options</DropdownMenuLabel>
-                                            <DropdownMenuSeparator />
-                                            <DropdownMenuItem className="cursor-pointer">Contact</DropdownMenuItem>
-                                            <DropdownMenuItem className="cursor-pointer">Add to team</DropdownMenuItem>
-                                            <DropdownMenuItem className="cursor-pointer">Block</DropdownMenuItem>
-                                        </DropdownMenuContent>
-                                    </DropdownMenu>
-                                </div>
-                            </div>
-                            <div className="flex items-center gap-2 mt-2 text-neutral-400 font-medium"><MapPin size={16} /> Dhaka, Bangladesh</div>
-                            <div className="flex gap-4 mt-4">
-                            {
-                                titles.map((title) => (
-                                    <span className="bg-neutral-100 dark:bg-neutral-900 text-neutral-500 px-4 py-2 rounded-full font-semibold">{title}</span>
-                                ))
-                            }
-                            </div>
-                            <div className="w-full mt-8">
-                                <h2 className="font-semibold text-2xl">A little about me</h2>
-                                <p className="mt-4 text-lg">
-                                    I'm a full-stack developer and I'm interested in joining a team to start a new project.
-                                </p>
+                    <div className="md:mx-8">
+                        <div className="w-full flex justify-between items-center">
+                            <div className="text-2xl md:text-5xl font-bold flex items-center gap-4 max-w-4/5">Ishtehar Hussain <span className="text-sm bg-primary text-background px-2 py-0.5 rounded">PRO</span></div>
+                            <div className="flex gap-1 items-center">
+                                <span className="hidden lg:inline mr-3 text-sm">Let's build something together</span>
+                                <Button className="hidden lg:inline cursor-pointer">Get in touch</Button>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button className="cursor-pointer" variant="outline" size="icon"><EllipsisVertical /></Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent sideOffset={10} className="dark:bg-neutral-900" align="end">
+                                        <DropdownMenuLabel>Options</DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem className="cursor-pointer">Contact</DropdownMenuItem>
+                                        <DropdownMenuItem className="cursor-pointer">Add to team</DropdownMenuItem>
+                                        <DropdownMenuItem className="cursor-pointer">Block</DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
                             </div>
                         </div>
-                        
+                        <div className="flex items-center gap-2 mt-2 text-neutral-400 font-medium"><MapPin size={16} /> Dhaka, Bangladesh</div>
+                        <div className="flex flex-wrap gap-4 mt-4">
+                        {
+                            titles.map((title) => (
+                                <span className="bg-neutral-100 dark:bg-neutral-900 text-neutral-500 px-4 py-2 rounded-full font-semibold">{title}</span>
+                            ))
+                        }
+                        </div>
+                        <div className="w-full mt-8">
+                            <h2 className="font-semibold text-2xl">A little about me</h2>
+                            <p className="mt-4 text-lg">
+                                I'm a full-stack developer and I'm interested in joining a team to start a new project.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mt-10 md:mx-8">
+                        <Tabs defaultValue="account">
+                            <TabsList>
+                                <TabsTrigger value="account">Account</TabsTrigger>
+                                <TabsTrigger value="password">Password</TabsTrigger>
+                            </TabsList>
+                            <Separator />
+                            <TabsContent className="min-h-[25vh]" value="account">
+                                Account Tab
+                            </TabsContent>
+                            <TabsContent className="min-h-[25vh]" value="password">
+                                Password Tab
+                            </TabsContent>
+                        </Tabs>
                     </div>
                 </div>
                 
