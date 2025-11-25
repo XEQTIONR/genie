1d2725d33de80536e393b6d018d533543d6a72e0
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2025-11-26 00:40:19 +0600

    Format message regarding interest in opportunity

--- app/Notifications/OpportunityInquiryNotification.php
+++ app/Notifications/OpportunityInquiryNotification.php
@@ -5,7 +5,6 @@ namespace App\Notifications;
 use App\Models\Opportunity;
 use App\Models\OpportunityInquiry;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
@@ -38,13 +37,28 @@ class OpportunityInquiryNotification extends Notification
         $sender_name = $this->inquiry->name;
         $sender_email = $this->inquiry->email;
         $message = $this->inquiry->message;
+        $message = str_replace("\r\n", "\n", $message);
 
-        return (new MailMessage)
+        $mail = (new MailMessage)
             ->greeting('New Message Received')
             ->line("You have been sent a new message about: $title")
             ->line("Sender: $sender_name ($sender_email)")
-            ->line("Message:")
-            ->line($message);
+            ->line("Message:");
+        
+        $lines = explode("\n", $message);
+
+        $lengths = [];
+
+        foreach ($lines as $line) {
+            $lengths[] = strlen($line);
+            if (strlen($line) > 0) {
+                $mail = $mail->line($line);
+            }
+        }
+
+        $mail = $mail->line('You can contact this person by emailing them directly via the email listed.');
+
+        return $mail;
     }
 
     /**
